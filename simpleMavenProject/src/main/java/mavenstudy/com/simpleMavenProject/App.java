a62f0056536b8b52ed9781f56a02d9436e0dfe60
songzhanlong <song_zhanlong@hotmail.com>
2015-11-25 00:23:13 +0800

    添加源码

---
+++ simpleMavenProject/src/main/java/mavenstudy/com/simpleMavenProject/App.java
@@ -0,0 +1,13 @@
+package mavenstudy.com.simpleMavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
